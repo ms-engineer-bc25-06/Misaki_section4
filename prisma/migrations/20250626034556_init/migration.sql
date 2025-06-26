a8f37fdb5b2a214235bad28f36b0b9e0634e5f48
miiko <miiko@progate.com>
2025-06-26 12:51:58 +0900

    prisamaとMySQL

---
+++ prisma/migrations/20250626034556_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(3) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
+    `amount` INTEGER NOT NULL,
+    `memo` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
